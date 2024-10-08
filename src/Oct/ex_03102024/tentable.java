7224db7b2f05d41afcd2c1cd179c9bfbeeddc2d8
pranavj247 <pranavjadhav831@gmail.com>
2024-10-09 01:47:48 +0530

    commit

---
+++ src/Oct/ex_03102024/tentable.java
@@ -0,0 +1,14 @@
+package Oct.ex_03102024;
+public class tentable
+{
+    public static void main(String[] args) {
+
+        for(int i=1;i<11;i++)
+        {
+            int j=10*i;
+
+            System.out.println("10*"+i+"="+j);
+        }
+
+    }
+}
