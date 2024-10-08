7bda2c28ff886a57c5ae1a7ff54f1d0ccb48206a
pranavj247 <pranavjadhav831@gmail.com>
2024-10-09 01:47:48 +0530

    commit

--- src/Oct/ex_0102024/Lab002.java
+++ src/Oct/ex_0102024/Lab002.java
@@ -1,6 +1,6 @@
 package Oct.ex_0102024;
 
-public class Lab001 {
+public class Lab002 {
     public static void main(String[] args) {
         System.out.println("tim");
 
--- src/lab001.java
+++ src/lab001.java
@@ -1,2 +1,9 @@
+import java.security.spec.RSAOtherPrimeInfo;
+
 public class lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hello worl");
+    }
+
 }
+
