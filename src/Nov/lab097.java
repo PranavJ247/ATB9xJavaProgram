e3dc598ec41521b36d86afececa30decd7ceca2d
pranavj247 <pranavjadhav831@gmail.com>
2024-11-04 01:36:53 +0530

    print Even no using For loop and If else

---
+++ src/Nov/lab097.java
@@ -0,0 +1,15 @@
+package Nov;
+
+public class lab097 {
+    public static void main(String[] args) {
+
+        int i = 1;
+        for (i=1;i<50;i++)
+        {
+            if (i % 2 == 0)
+            {
+            System.out.println(i);
+            }
+    }
+    }
+}
